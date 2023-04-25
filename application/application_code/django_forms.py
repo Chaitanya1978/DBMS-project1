da42d93617ec00d4b56a0acd3813b7982ff19878
Chaitanya1978 <131496782+Chaitanya1978@users.noreply.github.com>
2023-04-25 11:22:52 -0500

    Update django_forms.py
    adding the django forms for updating the member details and adding the member task details.

--- application/application_code/django_forms.py
+++ application/application_code/django_forms.py
@@ -9,3 +9,18 @@ class MemberCreationForm(forms.Form):
     email = forms.EmailField(max_length=254, required=True,widget=forms.TextInput(attrs={'placeholder': 'Email Address'}))
     mobile_number = forms.CharField(max_length=12, required=True,widget=forms.TextInput(attrs={'placeholder': 'Mobile Number'}))
     password = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+from django.contrib.auth.forms import UserChangeForm
+
+class MemberChangeForm(forms.Form):
+    first_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50, required=True)
+    email = forms.EmailField(max_length=254, required=True)
+    mobile_number = forms.CharField(max_length=12, required=True,)
+
+
+class MemberTasksForm(forms.Form):
+    name = forms.CharField(max_length=100,required=True,widget=forms.TextInput(attrs={'placeholder': 'Task Name'}))
+    due_date = forms.DateField(required=True,widget=forms.TextInput(attrs={'placeholder': 'MM/DD/YYYY'}))
+    is_task_completed = forms.BooleanField(required=False)
+    description = forms.CharField(max_length=200, required=False,widget=forms.TextInput(attrs={'placeholder': 'Task Description'}))
